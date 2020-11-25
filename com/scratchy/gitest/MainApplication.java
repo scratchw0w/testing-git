b733f351256740e558508cdabe5fb35b6380bce4
Max Basov <62623726+scratchw0w@users.noreply.github.com>
2020-11-25 16:22:24 +0200

    some changes

--- com/scratchy/gitest/MainApplication.java
+++ com/scratchy/gitest/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
  
     public static void main(String[] args) {
         System.out.println("Doing some staff..");
+        System.out.println("Some changes..");
     }
     
 }
